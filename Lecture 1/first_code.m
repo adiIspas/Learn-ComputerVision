421710f95339dd9e0904f14eea5e5c149f187786
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-15 14:36:22 +0300

    First code in Matlab

---
+++ Lecture 1/first_code.m
@@ -0,0 +1,69 @@
+A = [1, 2; ...
+        3, 4];
+RV = [1 2 3 4];
+CV = [1; 2; 3; 4];
+V = RV'
+
+B = [1:0.5:10];
+
+m = zeros(3,3);
+m(1,1)
+ 
+m(2, :)
+
+size(m,2)
+
+size(m,3)
+
+size(m,4)
+
+size(m)
+
+a = [1, 2, 3, 4, 5];
+mean(a)
+
+var(a)
+
+std(a)
+
+min(a)
+
+
+max(a)
+
+a = rand(3,2);
+b = rand(2,4);
+c = a * b
+ 
+d = rand(2,2)
+
+for i=1:1:7
+i
+end
+
+B = zeros(m,n);
+for i=1:m
+for j=1:n
+if A(i,j)>0
+B(i,j) = A(i,j);
+end
+end
+Undefined function or variable 'n'.
+ 
+B = zeros(3,3);
+for i=1:m
+for j=1:n
+if A(i,j)>0
+B(i,j) = A(i,j);
+end
+end
+A
+
+figure; 
+m = rand(64,64);
+imagesc(m) 
+colormap gray; 
+axis image; 
+axis off;
+I = imread('1.png');
+ 
