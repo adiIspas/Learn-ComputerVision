b66d48a81dc4a68b40e7b0affc9257d435e27daa
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-25 12:45:39 +0300

    Finish image transform

--- Lecture 1/imageTransform.m
+++ Lecture 1/imageTransform.m
@@ -7,7 +7,32 @@ A = [cos(theta) -sin(theta); sin(theta) cos(theta)];
 %Replace the identity matrix below with your transformation
 %matrix, and run this script.
 %%%%%%%%%%%% Your code here %%%%%%%%%%%%%%
-T = [1 0;0 1];
+T = [1 2;2 1];
+
+rads=90;
+
+%midpoints
+midx=ceil((size(img,1)+1)/2);
+midy=ceil((size(img,2)+1)/2);
+
+imagerot=zeros(size(img)); % midx and midy same for both
+
+for i=1:size(imagerot,1)
+    for j=1:size(imagerot,2)
+
+         x= (i-midx)*cos(rads)+(j-midy)*sin(rads);
+         y=-(i-midx)*sin(rads)+(j-midy)*cos(rads);
+         x=round(x)+midx;
+         y=round(y)+midy;
+
+         if (x>=1 && y>=1 && x<=size(img,1) && y<=size(img,2))
+              imagerot(i,j)=img(x,y);        
+         end
+    end
+end
+
+figure
+imshow(imagerot)
 
 %%%%%%%%%%%% end of your code %%%%%%%%%%%%%%
 
@@ -24,6 +49,7 @@ tform = maketform('projective',TWithTranslation');
 %Images are drawn with the +y axis pointing down. So flip the image before
 %and after the transformation to simulate normal Cartesian coordinates
 transformed = flipud(imtransform(flipud(img), tform, 'FillValues',255));
+figure
 imshow(img)
 figure
 imshow(uint8(transformed))
